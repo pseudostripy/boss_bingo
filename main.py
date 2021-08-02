0be3a6ee87195efe65c8165898be8e0af480d70f
pseudostripy <41550973+pseudostripy@users.noreply.github.com>
2021-08-02 12:39:33 +0100

    Add config file  - rework how user settings are loaded  - clean python code for more structured list setup

--- main.py
+++ main.py
@@ -1,70 +1,68 @@
 import random
+from configparser import ConfigParser
 
-FarmWep = open('ds2_farming_weapons.txt', 'r')
-PostWep = open('ds2_postshrine_weapons.txt', 'r')
-PreWep = open('ds2_preshrine_weapons.txt', 'r')
-TortWep = open('ds2_torture_weapons.txt', 'r')
-PostSpell = open('ds2_postshrine_spells.txt', 'r')
-PreSpell = open('ds2_preshrine_spells.txt', 'r')
-TortSpell = open('ds2_torture_spells.txt', 'r')
-PreBoss = open('ds2_preshrine_bosses.txt', 'r')
-PostBoss = open('ds2_postshrine_bosses.txt', 'r')
-DlcBoss = open('ds2_dlc_bosses.txt', 'r')
 
-IncFarmWep = input("include farming weapons? (y/n))").lower()
-IncPostWep = input("include post shrine weapons? (y/n))").lower()
-IncTortWep = input("include torture weapons? (y/n))").lower()
+# function for reading the .txt files
+def append_to_list(fname, inputlist):
+    fh = open(fname, 'r')
+    for name in fh.readlines():
+        inputlist.append(name[:-1])
+    return inputlist
 
-IncPreSpell = input("include spells? (y/n))").lower()
-IncPostSpell = input("include post shrine spells? (y/n))").lower()
-IncTortSpell = input("include torture spells? (y/n))").lower()
 
-IncPostBoss = input("include post shrine bosses? (y/n))").lower()
-IncDlcBoss = input("include dlc bosses? (y/n))").lower()
+# import user settings
+config = ConfigParser()
+config.read('bingoconfig.ini')
 
-wep = []
+bPredrang = config.getboolean('Options', 'preshrine board')
+bDLCbosses = config.getboolean('Options', 'dlc bosses')
+bSpells = config.getboolean('Options', 'spells')
+bFarm = config.getboolean('Options', 'farmed weapons')
+bTorture = config.getboolean('Options', 'torture')
+bUniqueBosses = config.getboolean('Options', 'Unique bosses')
 
-for i in PreWep.readlines():
-    wep.append(i[:-1])
-if IncFarmWep == "y":
-    for i in FarmWep.readlines():
-        wep.append(i[:-1])
-if IncTortWep == "y":
-    for i in TortWep.readlines():
-        wep.append(i[:-1])
-if IncTortWep == "y":
-    for i in TortWep.readlines():
-        wep.append(i[:-1])
-if IncPreSpell == "y":
-    for i in PreSpell.readlines():
-        wep.append(i[:-1])
-if IncPostSpell == "y":
-    for i in PostSpell.readlines():
-        wep.append(i[:-1])
-if IncTortSpell == "y":
-    for i in TortSpell.readlines():
-        wep.append(i[:-1])
-
-boss = []
-
-for i in PreBoss.readlines():
-    boss.append(i[:-1])
-if IncPostBoss == "y":
-    for i in PostBoss.readlines():
-        boss.append(i[:-1])
-if IncDlcBoss == "y":
-    for i in DlcBoss.readlines():
-        boss.append(i[:-1])
-to_out = "["
+# simple user traps:
+if bTorture and bPredrang:
+    print('Torture always includes post drangleic! Overriding.')
+    bPredrang = False
 
 nBingoSq = 25
-bAllowRepeatBosses = True
+
+# read bosses/weapons based on config
+wep = []
+boss = []
+
+# preshrine:
+wep = append_to_list('ds2_preshrine_weapons.txt', wep)
+boss = append_to_list('ds2_preshrine_bosses.txt', boss)
+if bDLCbosses:
+    boss = append_to_list('ds2_preshrine_dlc_bosses.txt', boss)
+if bSpells:
+    wep = append_to_list('ds2_preshrine_spells.txt', wep)
+# (this will fail if no dlc or post-shrine because the lists are not separated very well)
+if bFarm:
+    wep = append_to_list('ds2_farming_weapons.txt', wep)
+
+# post-shrine:
+if not bPredrang:
+    wep = append_to_list('ds2_postshrine_weapons.txt', wep)
+    boss = append_to_list('ds2_postshrine_bosses.txt', boss)
+
+    if bDLCbosses:
+        boss = append_to_list('ds2_postshrine_dlc_bosses.txt', boss)
+    if bSpells:
+        wep = append_to_list('ds2_postshrine_spells.txt', wep)
+    if bTorture:
+        wep = append_to_list('ds2_torture_weapons.txt', wep)
+        if bSpells:
+            wep = append_to_list('ds2_torture_spells.txt', wep)
+
 
 # randomise bosses and weapons
-if bAllowRepeatBosses:
-    randbosses = random.choices(boss, k=nBingoSq)
-else:
+if bUniqueBosses:
     randbosses = random.sample(boss, nBingoSq)
+else:
+    randbosses = random.choices(boss, k=nBingoSq)
 
 # currently always assume unique weapons
 randweapons = random.sample(wep, nBingoSq)
